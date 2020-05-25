c7dd1991b0b43dae76a9dac0d77920ebc945843e
guiguat <guaturadzn@gmail.com>
2020-05-25 01:42:48 -0300

    :sparkles: Include clock

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -29,12 +29,85 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(472, 353);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(464, 327);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Exercicio 01";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(464, 373);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Exercicio 02";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(175, 360);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(111, 29);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "22:54:04";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lblTimer.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(472, 398);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- App/Form1.cs
+++ App/Form1.cs
@@ -15,6 +15,40 @@ namespace App
         public Form1()
         {
             InitializeComponent();
+            timer1.Enabled = true;
+            
+        }
+
+        private int hora = DateTime.Now.Hour;
+        private int minuto = DateTime.Now.Minute;
+        private int segundo = DateTime.Now.Second;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTimer.Text = $"{hora}:{minuto}:{segundo}";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //incrementar as variaveis do tempo
+            segundo++;
+            if (minuto<60 && segundo == 60)
+            {
+                segundo = 0;
+                minuto++;
+            }
+            if(minuto == 60 && segundo == 60)
+            {
+                segundo = 0;
+                minuto = 0;
+                hora++;
+            }
+            lblTimer.Text = $"{hora}:{minuto}:{segundo}";
         }
     }
 }
